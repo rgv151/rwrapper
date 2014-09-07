487cc154abca28caeaa69a43277b6ef86f06e59e
NamPNQ <nampnq@gmail.com>
2014-09-07 17:06:50 +0700

    Reformat code

--- rwrapper/__init__.py
+++ rwrapper/__init__.py
@@ -8,174 +8,173 @@ import rethinkdb as r
 import json
 import jsonpickle
 
+
 class rwrapper(object):
+    id = None
 
-  id            = None
+    _limit = 0
+    _order_by = None
+    _meta = None
+    _changed = False
+    _pickle = False
+    _connection = None
+    _upsert = False
+    _non_atomic = True
 
-  _limit        = 0
-  _order_by     = None
-  _meta         = None
-  _changed      = False
-  _pickle       = False
-  _connection   = None
-  _upsert       = False
-  _non_atomic   = True
+    def __pickle__(self):
+        self._pickle = True
+        pickle = jsonpickle.encode(self)
+        self._pickle = False
+        return pickle
 
-  def __pickle__(self):
-    self._pickle = True
-    pickle = jsonpickle.encode(self)
-    self._pickle = False
-    return pickle
+    def __init__(self, **kwargs):
+        self._meta = {}
+        for key in dir(self):
+            try:
+                if key.startswith('_') and not key == '_connection':
+                    continue
+                value = getattr(self, key)
+                if not value.__rfield__:
+                    continue
+                self._meta[key] = value
+                self._meta[key].name = key
+                default = None
+                if not value.default == '_rwrapper_default':
+                    default = value.default
+                setattr(self, key, default)
+            except:
+                continue
+        for key in kwargs:
+            try:
+                setattr(self, key, self._meta[key].validate(kwargs[key]))
+            except:
+                setattr(self, key, kwargs[key])
 
-  def __init__(self, **kwargs):
-    self._meta = {}
-    for key in dir(self):
-      try:
-        if key.startswith('_') and not key == '_connection':
-          continue
-        value = getattr(self, key)
-        if not value.__rfield__:
-          continue
-        self._meta[key] = value
-        self._meta[key].name = key
-        default = None
-        if not value.default == '_rwrapper_default':
-          default = value.default
-        setattr(self, key, default)
-      except:
-        continue
-    for key in kwargs:
-      try:
-        setattr(self, key, self._meta[key].validate(kwargs[key]))
-      except:
-        setattr(self, key, kwargs[key])
+    def __json__(self):
+        d = {}
+        for key in dir(self):
+            try:
+                if not key == None and not key.startswith('_') and \
+                        not hasattr(getattr(self, key), '__call__'):
+                    d[key] = getattr(self, key)
+            except:
+                continue
+        return d
 
-  def __json__(self):
-    d = {}
-    for key in dir(self):
-      try:
-        if not key == None and not key.startswith('_') and \
-                                    not hasattr(getattr(self, key), '__call__'):
-          d[key] = getattr(self, key)
-      except:
-        continue
-    return d
+    def __setattr__(self, name, value):
+        try:
+            if not name.startswith('_') and not value == getattr(self, name):
+                self._changed = True
+        except:
+            pass
+        return object.__setattr__(self, name, value)
 
-  def __setattr__(self, name, value):
-    try:
-      if not name.startswith('_') and not value == getattr(self, name):
-        self._changed = True
-    except:
-      pass
-    return object.__setattr__(self, name, value)
+    def __getattribute__(self, name):
+        if name == '__dict__':
+            if not self._pickle:
+                return self.__json__()
+        return object.__getattribute__(self, name)
 
-  def __getattribute__(self, name):
-    if name == '__dict__':
-      if not self._pickle:
-        return self.__json__()
-    return object.__getattribute__(self, name)
+    def evaluate_insert(self, result):
+        if 'errors' in result and result['errors'] > 1:
+            raise IOError(json.dumps(result))
+        elif result['inserted'] == 1.0:
+            self.id = result['generated_keys'][0]
+        return self.id
 
-  def evaluate_insert(self, result):
-    if 'errors' in result and result['errors']>1:
-      raise IOError(json.dumps(result))
-    elif result['inserted'] == 1.0:
-      self.id = result['generated_keys'][0]
-    return self.id
+    def evaluate_update(self, result):
+        if 'updated' in result and result['updated'] == 0:
+            raise ValueError(json.dumps(result))
+        if 'replaced' in result and result['replaced'] == 0:
+            raise ValueError(json.dumps(result))
+        if 'errors' in result and result['errors'] > 0:
+            raise IOError(json.dumps(result))
+        return result
 
-  def evaluate_update(self, result):
-    if 'updated' in result and result['updated'] == 0:
-      raise ValueError(json.dumps(result))
-    if 'replaced' in result and result['replaced'] == 0:
-      raise ValueError(json.dumps(result))
-    if 'errors' in result and result['errors'] > 0:
-      raise IOError(json.dumps(result))
-    return result
+    def rq(self, filter=False):
+        if not filter:
+            filter = self._filter()
+        rq = r.table(self._db_table)
+        if len(filter) > 0:
+            rq = rq.filter(filter)
+        if not self._order_by == None:
+            rq = rq.order_by(*tuple([order if not order[:1] == '-' else \
+                                         r.desc(order[1:]) for order in list(self._order_by)]))
+        if not self._limit == 0:
+            rq = rq.limit(int(self._limit))
+        return rq
 
-  def rq(self, filter=False):
-    if not filter:
-      filter = self._filter()
-    rq = r.table(self._db_table)
-    if len(filter)>0:
-      rq = rq.filter(filter)
-    if not self._order_by == None:
-      rq = rq.order_by(*tuple([order if not order[:1] == '-' else \
-                        r.desc(order[1:]) for order in list(self._order_by) ]))
-    if not self._limit == 0:
-      rq = rq.limit(int(self._limit))
-    return rq
+    def _filter(self):
+        filter = {}
+        for key, value in self.__dict__.iteritems():
+            if key in self._meta and self._meta[key].default == value:
+                continue
+            if not value == None:
+                filter[key] = value
+        return filter
 
-  def _filter(self):
-    filter = {}
-    for key, value in self.__dict__.iteritems():
-      if key in self._meta and self._meta[key].default == value:
-        continue
-      if not value == None:
-        filter[key] = value
-    return filter
+    def all(self, o=False):
+        return [row if o == False else o(**row) for row in self.rq().run(self._connection)]
 
-  def all(self, o=False):
-    return [ row if o == False else o(**row) for row in self.rq().run(self._connection) ]
+    def get(self, o=False, exception=False):
+        try:
+            result = list(self.rq().limit(1).run(self._connection))[0]
+            if o == dict:
+                result = dict(result)
+            else:
+                result = result if o == False else o(**result)
+                if o:
+                    result.changed(False)
+            return result
+        except:
+            if exception == False:
+                return None
+            raise ValueError('Row not found in table.')
 
-  def get(self, o=False, exception=False):
-    try:
-      result = list(self.rq().limit(1).run(self._connection))[0]
-      if o == dict:
-        result = dict(result)
-      else:
-        result = result if o == False else o(**result)
-        if o:
-          result.changed(False)
-      return result
-    except:
-      if exception == False:
-        return None
-      raise ValueError('Row not found in table.')
+    def save(self):
+        # Try and be lazy about saving. Only save if our values have actually
+        # changed
+        if not self._changed:
+            return False
 
-  def save(self):
-    # Try and be lazy about saving. Only save if our values have actually
-    # changed
-    if not self._changed:
-      return False
+        # Validate any defined fields and set any defaults
+        doc = self.__dict__
+        if isinstance(self._meta, dict) and len(self._meta) > 0:
+            for key in self._meta.keys():
+                setattr(self, key, self._meta[key].validate(doc[key]))
 
-    # Validate any defined fields and set any defaults
-    doc = self.__dict__
-    if isinstance(self._meta, dict) and len(self._meta) > 0:
-      for key in self._meta.keys():
-        setattr(self, key, self._meta[key].validate(doc[key]))
-
-    # id being none means we should insert
-    if self.id == None:
-      doc = self.__dict__
-      if 'id' in doc:
-        del doc['id']
-      self.changed(False)
-      return self.evaluate_insert(r.table(self._db_table).insert(
-              doc,
-              upsert=self._upsert
+        # id being none means we should insert
+        if self.id == None:
+            if 'id' in doc:
+                del doc['id']
+            self.changed(False)
+            return self.evaluate_insert(r.table(self._db_table).insert(
+                self.__dict__,
+                upsert=self._upsert
             ).run(self._connection))
 
-    # id found; update
-    self.changed(False)
-    return self.evaluate_update(r.table(self._db_table).filter({'id': self.id}).update(
+        # id found; update
+        self.changed(False)
+        return self.evaluate_update(r.table(self._db_table).filter({'id': self.id}).update(
             self.__dict__,
             non_atomic=self._non_atomic
-          ).run(self._connection))
+        ).run(self._connection))
 
-  def changed(self, value):
-    if value == True or value == False:
-      self._changed = False
-    return self
+    def changed(self, value):
+        if value == True or value == False:
+            self._changed = False
+        return self
 
-  def order_by(self, *args):
-    self._order_by = args
-    return self
+    def order_by(self, *args):
+        self._order_by = args
+        return self
 
-  def limit(self, amount):
-    self._limit = amount
-    return self
+    def limit(self, amount):
+        self._limit = amount
+        return self
 
-  def count(self, filter=False):
-    return self.rq(filter).count().run(self._connection)
+    def count(self, filter=False):
+        return self.rq(filter).count().run(self._connection)
 
-  def delete(self, filter=False):
-    return self.rq(filter).delete().run(self._connection)
+    def delete(self, filter=False):
+        return self.rq(filter).delete().run(self._connection)
--- rwrapper/fields.py
+++ rwrapper/fields.py
@@ -6,204 +6,198 @@
 """
 import math
 
+
 def negative_field_check(should_check, value):
-  if not should_check:
-    return value
+    if not should_check:
+        return value
 
-  if not isinstance(value, (int, float, long)):
-    raise ValueError('%s field is the wrong type. Checked: int, long, float' % value)
+    if not isinstance(value, (int, float, long)):
+        raise ValueError('%s field is the wrong type. Checked: int, long, float' % value)
 
-  if not value < 0:
-      raise ValueError('%s field is not a negative value' % value)
+    if not value < 0:
+        raise ValueError('%s field is not a negative value' % value)
 
 
 def positive_field_check(should_check, value):
-  if not should_check:
-    return value
+    if not should_check:
+        return value
 
-  if not isinstance(value, (int, float, long)):
-    raise ValueError('%s field is the wrong type. Checked: int, long, float' % value)
+    if not isinstance(value, (int, float, long)):
+        raise ValueError('%s field is the wrong type. Checked: int, long, float' % value)
 
-  if not value > 0:
-      raise ValueError('%s field is not a positive value.' % value)
+    if not value > 0:
+        raise ValueError('%s field is not a positive value.' % value)
 
 
 class Field(object):
+    __rfield__ = True
 
-  __rfield__        = True
+    name = None
+    required = True
 
-  name              = None
-  required          = True
+    convert_type = True
+    default = '_rwrapper_default'
 
-  convert_type      = True
-  default           = '_rwrapper_default'
+    def __init__(self, **kwargs):
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
 
-  def __init__(self, **kwargs):
-    for key in kwargs:
-      setattr(self, key, kwargs[key])
+    def _name(self):
+        return self.name + '.' + self.__class__.__name__
 
-  def _name(self):
-    return self.name+'.'+self.__class__.__name__
+    def validate(self, value):
+        if value == None and not self.default == '_rwrapper_default':
+            return self.default
 
-  def validate(self, value):
-    if value == None and not self.default == '_rwrapper_default':
-      return self.default
+        if value == None and self.required == True:
+            raise ValueError('%s field is a required field. NoneType found.' % self._name())
+        return value
 
-    if value == None and self.required == True:
-      raise ValueError('%s field is a required field. NoneType found.' % self._name())
-    return value
+    def ensure_max_digits(self, value):
+        if not self.max_digits == None:
+            self.max_digits = int(self.max_digits)
+            if self.max_digits > 0 and not value < math.pow(10, self.max_digits) and \
+                    not value > -math.pow(10, self.max_digits):
+                raise ValueError('%s field size invalid. Constraint: maximum %d digits. Value: %d'
+                                 % (self._name(), self.max_digits))
+        return value
 
-  def ensure_max_digits(self, value):
-    if not self.max_digits == None:
-      self.max_digits = int(self.max_digits)
-      if self.max_digits > 0 and not value < math.pow(10, self.max_digits) and \
-                                  not value > -math.pow(10, self.max_digits):
-        raise ValueError('%s field size invalid. Constraint: maximum %d digits. Value: %d'
-                    % (self._name(), self.max_digits))
-    return value
-
-  def ensure_max_decimals(self, value):
-    # todo: try think of a better way to do this.
-    if not self.max_decimals == None and self.round_decimals == False:
-      decimals = str(value - int(value))
-      if decimals.startswith("0.") and len(decimals[2:]) > self.max_decimals:
-        raise ValueError("%s field size invalid. Constraint: maximum %d decimals. Value: %f"
-                      % (self._name(), self.max_decimals))
-    elif not self.max_decimals == None:
-      value = round(value, self.max_decimals)
-    return value
+    def ensure_max_decimals(self, value):
+        # todo: try think of a better way to do this.
+        if not self.max_decimals == None and self.round_decimals == False:
+            decimals = str(value - int(value))
+            if decimals.startswith("0.") and len(decimals[2:]) > self.max_decimals:
+                raise ValueError("%s field size invalid. Constraint: maximum %d decimals. Value: %f"
+                                 % (self._name(), self.max_decimals))
+        elif not self.max_decimals == None:
+            value = round(value, self.max_decimals)
+        return value
 
 
 class BooleanField(Field):
+    def validate(self, value):
+        value = super(BooleanField, self).validate(value)
 
-  def validate(self, value):
-    value = super(BooleanField, self).validate(value)
+        if not self.convert_type:
+            if not isinstance(value, bool):
+                raise ValueError('%s field is not String type.' % self._name())
+        else:
+            if int(value) == 0:
+                value = False
+            elif int(value) == 1:
+                value = True
+            else:
+                raise ValueError('%s field could not be converted to Boolean. Expected 0 or 1, found: %r'
+                                 % (self._name(), value))
 
-    if not self.convert_type:
-      if not isinstance(value, bool):
-        raise ValueError('%s field is not String type.' % self._name())
-    else:
-      if int(value) == 0:
-        value = False
-      elif int(value) == 1:
-        value = True
-      else:
-        raise ValueError('%s field could not be converted to Boolean. Expected 0 or 1, found: %r'
-                            % (self._name(), value))
-
-    return value
+        return value
 
 
 class CharField(Field):
+    max_length = None
+    min_length = None
 
-  max_length        = None
-  min_length        = None
+    utf8 = True
 
-  utf8              = True
+    def validate(self, value):
+        value = super(CharField, self).validate(value)
 
-  def validate(self, value):
-    value = super(CharField, self).validate(value)
+        if not self.convert_type:
+            if not isinstance(value, types.StringTypes):
+                raise ValueError('%s field is not String type.' % self._name())
+        elif not value == None:
+            try:
+                value = str(value if not self.utf8 else value.encode('utf8'))
+            except:
+                raise ValueError('%s field is not a String type.' % self._name())
 
-    if not self.convert_type:
-      if not isinstance(value, types.StringTypes):
-        raise ValueError('%s field is not String type.' % self._name())
-    elif not value == None:
-      try:
-        value = str(value if not self.utf8 else value.encode('utf8'))
-      except:
-        raise ValueError('%s field is not a String type.' % self._name())
+        if not self.min_length == None and len(value) < self.min_length:
+            raise ValueError('%s field too small. Constrained to maximum %d chars. Currently %d: "%s"'
+                             % (self._name(), self.min_length, len(value), value))
 
-    if not self.min_length == None and len(value) < self.min_length:
-      raise ValueError('%s field too small. Constrained to maximum %d chars. Currently %d: "%s"'
-                    % (self._name(), self.min_length, len(value), value))
+        if not self.max_length == None and len(value) > self.max_length:
+            raise ValueError('%s field too long. Constrained to maximum %d chars. Currently %d: "%s"'
+                             % (self._name(), self.max_length, len(value),
+                                value[:20] + ' [...]' if len(value) > 20 else value))
 
-    if not self.max_length == None and len(value) > self.max_length:
-      raise ValueError('%s field too long. Constrained to maximum %d chars. Currently %d: "%s"'
-                    % (self._name(), self.max_length, len(value),
-                          value[:20] + ' [...]' if len (value) > 20 else value))
-
-    return value
+        return value
 
 
 class LongField(Field):
+    positive_only = False
+    negative_only = False
+    round_decimals = True  # todo: implement this
+    max_digits = None
 
-  positive_only     = False
-  negative_only     = False
-  round_decimals    = True #todo: implement this
-  max_digits        = None
+    def validate(self, value):
+        value = super(LongField, self).validate(value)
 
-  def validate(self, value):
-    value = super(LongField, self).validate(value)
+        if not self.convert_type:
+            if not isinstance(value, long):
+                raise ValueError('%s field is not Long type.' % self._name())
+        elif not value == None:
+            try:
+                value = long(value)
+            except:
+                raise ValueError('Error trying to convert %s to Long type.' % self._name())
 
-    if not self.convert_type:
-      if not isinstance(value, long):
-        raise ValueError('%s field is not Long type.' % self._name())
-    elif not value == None:
-      try:
-        value = long(value)
-      except:
-        raise ValueError('Error trying to convert %s to Long type.' % self._name())
+        negative_field_check(self.negative_only, value)
+        positive_field_check(self.positive_only, value)
 
-    negative_field_check(self.negative_only, value)
-    positive_field_check(self.positive_only, value)
-
-    return self.ensure_max_digits(value)
+        return self.ensure_max_digits(value)
 
 
 class IntegerField(Field):
+    positive_only = False
+    negative_only = False
+    max_digits = None
 
-  positive_only     = False
-  negative_only     = False
-  max_digits        = None
+    def validate(self, value):
+        value = super(IntegerField, self).validate(value)
 
-  def validate(self, value):
-    value = super(IntegerField, self).validate(value)
+        if not self.convert_type:
+            if not isinstance(value, int):
+                raise ValueError('%s field is not Integer type.' % self._name())
+        elif not value == None:
+            value = int(value)
 
-    if not self.convert_type:
-      if not isinstance(value, int):
-        raise ValueError('%s field is not Integer type.' % self._name())
-    elif not value == None:
-      value = int(value)
+        negative_field_check(self.negative_only, value)
+        positive_field_check(self.positive_only, value)
 
-    negative_field_check(self.negative_only, value)
-    positive_field_check(self.positive_only, value)
-
-    return self.ensure_max_digits(value)
+        return self.ensure_max_digits(value)
 
 
 class FloatField(Field):
+    negative_only = False
+    positive_only = False
+    max_digits = None
+    max_decimals = None
+    round_decimals = False
 
-  negative_only     = False
-  positive_only     = False
-  max_digits        = None
-  max_decimals      = None
-  round_decimals    = False
+    def validate(self, value):
+        value = super(FloatField, self).validate(value)
 
-  def validate(self, value):
-    value = super(FloatField, self).validate(value)
+        if not self.convert_type:
+            if not isinstance(value, float):
+                raise ValueError('%s field is not Float type.' % self._name())
+        elif not value == None:
+            value = float(value)
 
-    if not self.convert_type:
-      if not isinstance(value, float):
-        raise ValueError('%s field is not Float type.' % self._name())
-    elif not value == None:
-      value = float(value)
+        negative_field_check(self.negative_only, value)
+        positive_field_check(self.positive_only, value)
 
-    negative_field_check(self.negative_only, value)
-    positive_field_check(self.positive_only, value)
+        if not self.max_digits == None and self.max_digits > 0:
+            IntegerField(name=self._name(), max_digits=self.max_digits).validate(value)
 
-    if not self.max_digits == None and self.max_digits > 0:
-      IntegerField(name=self._name(), max_digits=self.max_digits).validate(value)
-
-    return self.ensure_max_decimals(value)
+        return self.ensure_max_decimals(value)
 
 
 class ObjectField(Field):
+    def validate(self, value):
+        if type(value) is not dict and type(value) is not list:
+            raise ValueError('%s field is not dict or list type. Found type: %s' % (
+                self._name(),
+                type(value))
+            )
 
-  def validate(self, value):
-    if type(value) is not dict and type(value) is not list:
-      raise ValueError('%s field is not dict or list type. Found type: %s' % (
-        self._name(), 
-        type(value))
-      )
-
-    return value
+        return value
